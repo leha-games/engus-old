e2286cba7514f24b3ea880b6fed9bca1a448f314
Alexey Grigoriev <grialexey@gmail.com>
2013-12-24 19:40:46 +0400

    Add UserWord model to account app

--- english/apps/account/models.py
+++ english/apps/account/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from english.apps.dictionary.models import Word
 
 
-class UserWords(models.Model):
-    user = models.OneToOneField(User)
-    words = models.ManyToManyField(Word)
+class UserWord(models.Model):
+    user = models.ForeignKey(User)
+    word = models.ForeignKey(Word)
+    know = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    last_repeat = models.DateTimeField(null=True, blank=True)
+    repeated = models.SmallIntegerField(default=0)
+    mistakes = models.SmallIntegerField(default=0)
+
+    class Meta:
+        unique_together = ('user', 'word') 
