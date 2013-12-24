8607d50f9e5f67a95f43d2493970562a56c9d69b
Alexey Grigoriev <grialexey@gmail.com>
2013-12-24 23:43:24 +0400

    Refactoring and font awesome add

--- english/static/js/app/app.js
+++ english/static/js/app/app.js
@@ -12,17 +12,17 @@ angular.module('engusApp', ['restangular', 'ui.router'])
                 controller: 'DictionaryCtrl as dict',
                 resolve: {
                     Dictionary: function($http) {
-                        return $http.get('/dictionary/').success(function(result) { return result.data });
+                        return $http.get('/dictionary/');
                     }
                 }
             })
             .state('dictionary.word', {
                 url: ':word',
                 templateUrl: '/static/js/app/templates/dictionary.word.html',
-                controller: 'DictionaryWordCtrl as word',
+                controller: 'DictionaryWordCtrl as WordCtrl',
                 resolve: {
                     DictionaryWord: function($http, $stateParams) {
-                        return $http.get('/dictionary/' + $stateParams.word).success(function(result) { return result.data });
+                        return $http.get('/dictionary/' + $stateParams.word);
                     }
                 }
             });
