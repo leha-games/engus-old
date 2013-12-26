3c4112154cdf2d35c87e569a89a3cf7e3f36007a
Alexey Grigoriev <grialexey@gmail.com>
2013-12-26 15:41:10 +0400

    Rename account app into cards app

--- english/apps/cards/models.py
+++ english/apps/cards/models.py
@@ -6,11 +6,11 @@ from english.apps.dictionary.models import Word
 class UserWord(models.Model):
     user = models.ForeignKey(User)
     word = models.ForeignKey(Word)
-    know = models.BooleanField(default=False)
+    learning = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     last_repeat = models.DateTimeField(null=True, blank=True)
-    repeated = models.SmallIntegerField(default=0)
-    mistakes = models.SmallIntegerField(default=0)
+    repeat_level = models.SmallIntegerField(default=0)
+    next_repeat = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         unique_together = ('user', 'word') 
