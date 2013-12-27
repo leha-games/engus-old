7db3871adb784f99ea3fb38095b565a23dfafa9c
Alexey Grigoriev <grialexey@gmail.com>
2013-12-27 12:28:25 +0400

    Example model in dictionary app migration

---
+++ english/apps/dictionary/migrations/0009_auto__add_example__del_field_definition_example_illustration__del_fiel.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Example'
+        db.create_table(u'dictionary_example', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('definition', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['dictionary.Definition'])),
+            ('text', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('russian_translation', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('illustration', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('level', self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal(u'dictionary', ['Example'])
+
+        # Deleting field 'Definition.example_illustration'
+        db.delete_column(u'dictionary_definition', 'example_illustration')
+
+        # Deleting field 'Definition.example_russian_translation'
+        db.delete_column(u'dictionary_definition', 'example_russian_translation')
+
+        # Deleting field 'Definition.example'
+        db.delete_column(u'dictionary_definition', 'example')
+
+
+    def backwards(self, orm):
+        # Deleting model 'Example'
+        db.delete_table(u'dictionary_example')
+
+        # Adding field 'Definition.example_illustration'
+        db.add_column(u'dictionary_definition', 'example_illustration',
+                      self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Definition.example_russian_translation'
+        db.add_column(u'dictionary_definition', 'example_russian_translation',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=255, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Definition.example'
+        db.add_column(u'dictionary_definition', 'example',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=255, blank=True),
+                      keep_default=False)
+
+
+    models = {
+        u'dictionary.definition': {
+            'Meta': {'object_name': 'Definition'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'definition': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'illustration': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'part_of_speach': ('django.db.models.fields.SmallIntegerField', [], {}),
+            'russian_definition': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'weight': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'}),
+            'word': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['dictionary.Word']"})
+        },
+        u'dictionary.example': {
+            'Meta': {'object_name': 'Example'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'definition': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['dictionary.Definition']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'illustration': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'russian_translation': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'text': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'dictionary.word': {
+            'Meta': {'ordering': "['word']", 'object_name': 'Word'},
+            'audio': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'mueller_definition': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'short_translation': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'transcription': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'weight': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'word': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'})
+        },
+        u'dictionary.wordform': {
+            'Meta': {'object_name': 'WordForm'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'form': ('django.db.models.fields.SmallIntegerField', [], {}),
+            'headword': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'headword_for_words'", 'null': 'True', 'to': u"orm['dictionary.Word']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'word': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['dictionary.Word']"})
+        }
+    }
+
+    complete_apps = ['dictionary']
