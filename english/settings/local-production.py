ef7604102d3b6577388fc707b6b608ff6e230397
Alexey Grigoriev <grialexey@gmail.com>
2014-01-03 20:31:15 +0400

    Production settings and base template editing

--- english/settings/local-production.py
+++ english/settings/local-production.py
@@ -20,7 +20,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'english',                        # Or path to database file if using sqlite3.
+        'NAME': 'engusdb',                        # Or path to database file if using sqlite3.
         'USER': os.environ['DATABSE_USER'],                  # Not used with sqlite3.
         'PASSWORD': os.environ['DATABASE_PASSWORD'],         # Not used with sqlite3.
         'HOST': '',                                          # Set to empty string for localhost. Not used with sqlite3.
@@ -33,7 +33,7 @@ DATABASES = {
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/var/www/english/media/'
+MEDIA_ROOT = '/var/www/engus/media/'
 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
@@ -46,7 +46,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/www/english/static/'
+STATIC_ROOT = '/var/www/engus/static/'
 
 
 # URL prefix for static files.
@@ -82,6 +82,3 @@ LOGGING = {
         },
     }
 }
-
-
-# Third-party apps settings
