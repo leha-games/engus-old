2ad9db9251e248e0b957d4b1ea1beefcdfe7badb
Alexey Grigoriev <grialexey@gmail.com>
2014-01-13 00:04:24 +0400

    Redesigned Dictionary admin

--- engus/apps/dictionary/admin.py
+++ engus/apps/dictionary/admin.py
@@ -4,14 +4,6 @@ from django.db import models
 from .models import Word, Definition, Example
 
 
-class ExampleAdmin(admin.ModelAdmin):
-    list_display = ('definition', 'created', 'modified', )
-    ordering = ['-modified', ]
-    formfield_overrides = {
-        models.CharField: {'widget': TextInput(attrs={'size':'150'})},
-    }
-
-
 class ExampleInline(admin.StackedInline):
     model = Example
     extra = 1
@@ -21,7 +13,7 @@ class ExampleInline(admin.StackedInline):
     
 
 class DefinitionAdmin(admin.ModelAdmin):
-    list_display = ('word', 'part_of_speach', 'definition', 'weight', )
+    list_display = ('__unicode__', 'word', 'part_of_speach', 'definition', 'weight', )
     search_fields = ['word', ]
     inlines = [
         ExampleInline,
@@ -49,5 +41,4 @@ class WordAdmin(admin.ModelAdmin):
     ]
     
 admin.site.register(Word, WordAdmin)
-admin.site.register(Example, ExampleAdmin)
 admin.site.register(Definition, DefinitionAdmin)
