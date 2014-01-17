5fcf3aad9fd26f55167d7c97dff8bb3a16383d22
Alexey Grigoriev <grialexey@gmail.com>
2014-01-17 23:34:33 +0400

    Migration for Definition and Example models'

--- engus/apps/dictionary/migrations/0007_auto__chg_field_definition_label__chg_field_example_label.py
+++ engus/apps/dictionary/migrations/0007_auto__chg_field_definition_label__chg_field_example_label.py
@@ -8,56 +8,24 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding field 'Definition.explanation'
-        db.add_column(u'dictionary_definition', 'explanation',
-                      self.gf('django.db.models.fields.CharField')(default='', max_length=100, blank=True),
-                      keep_default=False)
-
-
-        # Changing field 'Definition.label'
-        db.delete_column(u'dictionary_definition', 'label')
-        db.add_column(u'dictionary_definition', 'label', self.gf('django.db.models.fields.SmallIntegerField')(null=True))
-        # Deleting field 'Word.weight'
-        db.delete_column(u'dictionary_word', 'weight')
-
-        # Deleting field 'Word.is_public'
-        db.delete_column(u'dictionary_word', 'is_public')
-
-        # Deleting field 'Word.short_translation'
-        db.delete_column(u'dictionary_word', 'short_translation')
-
-        # Adding field 'Example.label'
-        db.add_column(u'dictionary_example', 'label',
-                      self.gf('django.db.models.fields.SmallIntegerField')(null=True, blank=True),
-                      keep_default=False)
-
-
-    def backwards(self, orm):
-        # Deleting field 'Definition.explanation'
-        db.delete_column(u'dictionary_definition', 'explanation')
-
 
         # Changing field 'Definition.label'
         db.delete_column(u'dictionary_definition', 'label')
         db.add_column(u'dictionary_definition', 'label', self.gf('django.db.models.fields.CharField')(default='', max_length=100))
-        # Adding field 'Word.weight'
-        db.add_column(u'dictionary_word', 'weight',
-                      self.gf('django.db.models.fields.PositiveIntegerField')(unique=True, null=True, blank=True),
-                      keep_default=False)
 
-        # Adding field 'Word.is_public'
-        db.add_column(u'dictionary_word', 'is_public',
-                      self.gf('django.db.models.fields.BooleanField')(default=False),
-                      keep_default=False)
-
-        # Adding field 'Word.short_translation'
-        db.add_column(u'dictionary_word', 'short_translation',
-                      self.gf('django.db.models.fields.CharField')(default='', max_length=64, blank=True),
-                      keep_default=False)
-
-        # Deleting field 'Example.label'
+        # Changing field 'Example.label'
         db.delete_column(u'dictionary_example', 'label')
+        db.add_column(u'dictionary_example', 'label', self.gf('django.db.models.fields.CharField')(default='', max_length=100))
 
+    def backwards(self, orm):
+
+        # Changing field 'Definition.label'
+        db.delete_column(u'dictionary_definition', 'label')
+        db.add_column(u'dictionary_definition', 'label', self.gf('django.db.models.fields.SmallIntegerField')(null=True))
+
+        # Changing field 'Example.label'
+        db.delete_column(u'dictionary_example', 'label')
+        db.add_column(u'dictionary_example', 'label', self.gf('django.db.models.fields.SmallIntegerField')(null=True))
 
     models = {
         u'dictionary.definition': {
@@ -67,11 +35,12 @@ class Migration(SchemaMigration):
             'explanation': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'illustration': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            'label': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
             'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'part_of_speach': ('django.db.models.fields.SmallIntegerField', [], {}),
             'russian_definition': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
             'weight': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'}),
+            'where_used': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
             'word': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['dictionary.Word']"})
         },
         u'dictionary.example': {
@@ -80,7 +49,7 @@ class Migration(SchemaMigration):
             'definition': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['dictionary.Definition']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'illustration': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            'label': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
             'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'russian_translation': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
             'text': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
