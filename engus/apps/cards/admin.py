6fa1732a67d05fca2c105203c1e9917c5c21e551
Alexey Grigoriev <grialexey@gmail.com>
2014-05-23 13:01:31 +0400

    Cards list filter in admin

--- engus/apps/cards/admin.py
+++ engus/apps/cards/admin.py
@@ -4,6 +4,7 @@ from .models import Card
 
 class CardAdmin(admin.ModelAdmin):
     list_display = ('user', 'word', 'level', 'status', 'when_learned', 'created', )
+    list_filter = ('status', )
     readonly_fields = ('word', )
 
 
