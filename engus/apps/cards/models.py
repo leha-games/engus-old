84656b6dcaf39aa653efa230ac32c17ba1426b62
Alexey Grigoriev <grialexey@gmail.com>
2014-05-23 11:35:07 +0400

    Add status field to Card model

--- engus/apps/cards/models.py
+++ engus/apps/cards/models.py
@@ -5,10 +5,19 @@ from engus.apps.dictionary.models import Word
 
 
 class Card(models.Model):
+    NEW = 'new'
+    LEARNED = 'learned'
+    KNOW = 'know'
+    STATUS_CHOICES = (
+        (NEW, 'New'),
+        (LEARNED, 'Leaned'),
+        (KNOW, 'Know'),
+    )
     user = models.ForeignKey(User)
     word = models.ForeignKey(Word)
     created = models.DateTimeField(auto_now_add=True)
     learned = models.BooleanField(default=False)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=NEW)
     when_learned = models.DateTimeField(null=True, blank=True)
     level = models.PositiveIntegerField(default=0)
 
